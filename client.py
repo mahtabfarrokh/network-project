6ef1daea7d2183defaed81626bc424267c268c76
mahtabfarrokh <mahtab.farrokh@gmail.com>
2018-07-05 22:57:51 +0430

    try to fragment

--- client.py
+++ client.py
@@ -2,9 +2,9 @@ import socket
 import time
 import requests
 
-turn = 0
-IP = '192.168.1.33'
-# IP = '192.168.1.55'
+turn = 1
+# IP = '192.168.1.33'
+IP = '192.168.1.55'
 
 
 
@@ -16,40 +16,51 @@ if turn:
     NS = 0
     MF = 0  # More fragment
     iteration = 2
-    data = 'GET / HTTP/1.0\r\n\r\n'
+    data = 'GET / HTTP/1.0'
     newdata = ''
+    realdata = ''
+    realdata1 = ''
 
-
-    def checksum1(MESSAGE):
+    def checksum1(message):
         c = 0
-
-        for x in MESSAGE:
+        for x in message:
             c = c + ord(x)
-        checksum = bin(c)
-        checksum = checksum.split('b')
+        csum = bin(c)
+        csum = csum.split('b')
 
-        return checksum[1]
+        return csum[1]
 
-
-    if len(data) > 65000:
-        iteration = len(data) / 65000 + 2
+    segment_size = 5
+    print("leeeeeeeeeeeeen:")
+    print(len(data))
+    if len(data) > segment_size:
+        iteration = int(len(data) / segment_size) + 2
         MF = 1
-
+        print("fragment happened")
+    else:
+        MF = 0
     for i in range(1, iteration):
         UDP_PORT = 5016
         if i == iteration - 1:
             MF = 0
         print('UDP target IP:', UDP_IP)
         print('UDP target port:', UDP_PORT)
-        start = (i - 1) * 65000
-        end = i * 65000
+        start = (i - 1) * segment_size
+        end = i * segment_size
+        if end > len(data):
+            end = len(data)
+        print(address)
+        print(port)
+        print(NS)
+        print(MF)
+        print(data)
         msg = address + '@' + port + '@' + str(NS) + '@' + str(MF) + '@' + data[start:end]
-        # MESSAGE = bytes(msg, 'utf-8')
-
         print('HERE', msg)
-        cmsg = msg.split('\r\n\r\n')
-        print(cmsg[0])
-        checksum = checksum1(cmsg[0])
+        cmsg = msg.split('\\')
+        print('check', msg)
+        # checksum = checksum1(cmsg[0])
+        checksum = checksum1(msg)
+        msg = msg + '\r\n\r\n'
         newmsg = msg + '@' + str(checksum)
         MESSAGE = bytes(newmsg, 'utf-8')
 
@@ -58,54 +69,82 @@ if turn:
                              socket.SOCK_DGRAM)  # UDP
         sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
         sock.close()
+        counter = 0
         while True:
+            counter += 1
+            print("counter :", counter)
             UDP_PORT = 5008
             sock = socket.socket(socket.AF_INET,  # Internet
                                  socket.SOCK_DGRAM)  # UDP
             sock.bind((UDP_IP, UDP_PORT))
-            sock.settimeout(0.00001)
+            sock.settimeout(1)
             try:
-                data, addr = sock.recvfrom(1024)
+                data2, addr = sock.recvfrom(1024)
+                NR = str(data2)[2]
+                print('ack : ', NR, NS)
+                sock.close()
+                print("NR & NS :", NR, int(not bool(NS)))
+                if int(NR) == int(not bool(NS)):
+                    NS = int(NR)
+                    print("aaaaaalaaaaaaaah ooo akkkbaaaarrrrrr")
+                    break
+
+                print("received data:", NR)
             except socket.timeout:
                 print('timeout')
-
-            NR = int(data)
-            print('ack : ', NR, NS)
-            sock.close()
-            if NR == int(not bool(NS)):
-                NS = NR
-                break
-            else:
+                print('retransmit: ', MESSAGE)
                 UDP_PORT = 5016
                 sock = socket.socket(socket.AF_INET,  # Internet
                                      socket.SOCK_DGRAM)  # UDP
                 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
                 sock.close()
 
-            print("received data:", NR)
-
     while True:
-
-        UDP_PORT = 5007
+        UDP_PORT = 5017
         sock = socket.socket(socket.AF_INET,  # Internet
                              socket.SOCK_DGRAM)  # UDP
         sock.bind((UDP_IP, UDP_PORT))
         data, addr = sock.recvfrom(1024)
-        newdata = str(data)
-        newdata = newdata.split('\'')
-        newdata2 = newdata[1].split('@')
-        checks = checksum1(newdata2[0])
-        print('received data:', data)
         sock.close()
-        break
+
+        if data:
+            data = str(data).split("@")
+            print(data)
+            NS = int(data[0][2:])
+            MF = int(data[1])
+            MESSAGE = data[0][2:] + '@' + data[1] + '@' + data[2]
+            # cmsg = realdata1.split('\\')
+            # print(cmsg)
+            # cacheSaveMsg = realdata1.split('\\')[0]
+
+            print("received message:", MESSAGE)
+            print("N Next ", NR)
+            print(MESSAGE)
+            print('checksum', data[3][:-9], checksum1(MESSAGE))
+            if NS == int(not bool(NR)) and (checksum1(MESSAGE) == data[3][:-9]):
+                print("heeeeeeeeeeeeereeeeeeeeeeeeee")
+                realdata = realdata + str(data[2][:-8])
+                UDP_PORT = 5008
+                ack = bytes(str(NR), 'utf-8')
+                print("NR:", ack, NR, NS)
+                sock = socket.socket(socket.AF_INET,  # Internet
+                                     socket.SOCK_DGRAM)  # UDP
+                sock.sendto(ack, (UDP_IP, UDP_PORT))
+                sock.close()
+                NR = NS
+                print("******************************")
+            if MF == 0:
+                break
+    realdata += '\r\n\r\n'
+    print("real data : ", realdata)
 
 else:
-    type = 'CNAME'
+    dnstype = 'CNAME'
     target = 'www.soft98.ir'
     TCP_IP = bytes(IP, 'utf-8')
-    TCP_PORT = 5012
+    TCP_PORT = 5013
     BUFFER_SIZE = 1024
-    MESSAGE = bytes(type + '@' + target + '@', 'utf-8')
+    MESSAGE = bytes(dnstype + '@' + target + '@', 'utf-8')
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((TCP_IP, TCP_PORT))
--- proxy.py
+++ proxy.py
@@ -1,19 +1,20 @@
 import socket
 import time
-# import dns.resolver
+import dns.resolver
 import requests
 
-# IP = '192.168.1.55'
+IP = '192.168.1.55'
 # IP = "192.168.80.1"
-IP = '192.168.1.33'
+# IP = '192.168.1.33'
 UDP_IP = bytes(IP, 'utf-8')
 UDP_PORT = 5017
 TCP_PORT = 80
 TCP_IP = ''
 realdata = ''
 realdata1 = ''
+cacheSaveMsg = ''
 NR = 1
-turn = 0
+turn = 1
 index = 0
 dnsindex = 0
 httpCache = []
@@ -21,10 +22,10 @@ DNSCache = []
 inCache = 0
 inDNSCache = 0
 
-def checksum(MESSAGE):
-    c = 0
 
-    for x in MESSAGE:
+def checksum(message):
+    c = 0
+    for x in message:
         c = c + ord(x)
     csum = bin(c)
     csum = csum.split('b')
@@ -32,55 +33,124 @@ def checksum(MESSAGE):
 
 
 
-def responseToClient(data) :
-
-    UDP_PORT = 5007
+def response_to_client(data):
+    print("------------------>>>>>>>")
+    print(data)
+    udp_port = 5017
     sock = socket.socket(socket.AF_INET,  # Internet
                          socket.SOCK_DGRAM)  # UDP
 
     newdata = str(data)
     newdata = newdata.split('\'')
     splitedData = newdata[1].split(' ')
-    responseType = splitedData[1]
-    # print('here',responseType)
-
-    if int(responseType) == 200:
-        # checksum = checksum(newdata[1])
-        # newdata2 = bytes(newdata[1] + '@' + str(checksum), 'utf_8')
+    response_type = splitedData[1]
+    # print('here',response_type)
+    NS = 0
+    MF = 0  # More fragment
+    iteration = 2
+    if int(response_type) == 200:
         print('ok ^^ , code = 200 !')
-        sock.sendto(data, (UDP_IP, UDP_PORT))
-        print("received data:", data)
-        sock.close()
+        # sock.sendto(data, (UDP_IP, udp_port))
+        # print("received data:", data)
+        # sock.close()
 
-    elif int(responseType) == 404:
+        segment_size = 15
+        print("leeeeeeeeeeeeen:")
+        print(len(data))
+        if len(data) > segment_size:
+            iteration = int(len(data) / segment_size) + 2
+            MF = 1
+            print("fragment happened")
+        else:
+                MF = 0
+
+        data = str(data)[2:-1]
+        print("====================")
+        print(data)
+        print("====================")
+        for i in range(1, iteration):
+            if i == iteration - 1:
+                MF = 0
+            start = (i - 1) * segment_size
+            end = i * segment_size
+            if end > len(data):
+                end = len(data)
+            print("====================")
+            print(data[start:end])
+            print("====================")
+            msg = str(NS) + '@' + str(MF) + '@' + data[start:end]
+            print('HERE', msg)
+            print('check', msg)
+            msg = msg.replace('\\n', '\n')
+            msg = msg.replace('\\r', '\r')
+            csum = checksum(msg)
+            # msg = msg + '\r\n\r\n'
+            newmsg = msg + '@' + str(csum) + '\r\n\r\n'
+            MESSAGE = bytes(newmsg, 'utf-8')
+
+            print('message with checksum :', MESSAGE)
+
+            sock.sendto(MESSAGE, (UDP_IP, udp_port))
+            sock.close()
+            counter = 0
+            while True:
+                counter += 1
+                print("counter :", counter)
+                UDP_PORT = 5008
+                sock = socket.socket(socket.AF_INET,  # Internet
+                                     socket.SOCK_DGRAM)  # UDP
+                sock.bind((UDP_IP, UDP_PORT))
+                sock.settimeout(1)
+                try:
+                    data2, addr = sock.recvfrom(1024)
+                    NR = str(data2)[2]
+                    print('ack : ', NR, NS)
+                    sock.close()
+                    print("NR & NS :", NR, int(not bool(NS)))
+                    if int(NR) == int(not bool(NS)):
+                        NS = int(NR)
+                        print("ssssaaaallllaaaavvvvaaaaatttt")
+                        break
+
+                    print("received data:", NR)
+                except socket.timeout:
+                    print('timeout')
+                    print('retransmit: ', MESSAGE)
+                    UDP_PORT = 5016
+                    sock = socket.socket(socket.AF_INET,  # Internet
+                                         socket.SOCK_DGRAM)  # UDP
+                    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+                    sock.close()
+
+    elif int(response_type) == 404:
         print('error not found , code = 404 !')
-        sock.sendto(bytes('error not found !','utf_8'), (UDP_IP, UDP_PORT))
+        sock.sendto(bytes('error not found !', 'utf_8'), (UDP_IP, udp_port))
         sock.close()
 
-    elif int(responseType) == 400:
+    elif int(response_type) == 400:
         print('bad req , code = 400 !')
-        sock.sendto(bytes('bad request !','utf_8'), (UDP_IP, UDP_PORT))
+        sock.sendto(bytes('bad request !', 'utf_8'), (UDP_IP, udp_port))
         sock.close()
 
-    elif int(responseType) == 301 or int(responseType) == 302:
+    elif int(response_type) == 301 or int(response_type) == 302:
         print('moved and redirect  , code = 301 or 302 !')
         for i in splitedData:
             if 'Location:' in i:
                 # print(splitedData[splitedData.index(i) + 1])
-                newLocation = splitedData[splitedData.index(i) + 1]
-                newLocation = newLocation.split('//')
-                newLocation = newLocation[1].split('\\')
-                new_IP = newLocation[0]
+                new_location = splitedData[splitedData.index(i) + 1]
+                new_location = new_location.split('//')
+                new_location = new_location[1].split('\\')
+                new_ip = new_location[0]
 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print((bytes(new_IP, 'utf-8')))
+        print((bytes(new_ip, 'utf-8')))
         # print(TCP_PORT)
-        s.connect((bytes(new_IP, 'utf-8'), TCP_PORT))
+        s.connect((bytes(new_ip, 'utf-8'), TCP_PORT))
         s.send(bytes(realdata, 'utf-8'))
         data = s.recv(BUFFER_SIZE)
         s.close()
 
-        responseToClient(data)
+        response_to_client(data)
 
 
 if turn:
@@ -101,22 +171,28 @@ if turn:
             TCP_PORT = int(data[1])
             NS = int(data[2])
             MF = int(data[3])
-            MESSAGE = bytes(data[4], 'utf-8')
-            realdata = realdata + str(data[4][:])
-            realdata1 = realdata1 + str(data[0][2:]) + '@' + str(data[1]) + '@' + str(data[2]) + '@' + str(
-                data[3]) + '@' + str(data[4])
-
-            cmsg = realdata1.split('\\')
-            cacheSaveMsg = realdata1.split('\\')[0]
+            MESSAGE = data[0][2:] + '@' + data[1] + '@' + data[2] + '@' + data[3] + '@' + data[4][:-8]
+            # cmsg = realdata1.split('\\')
+            # print(cmsg)
+            # cacheSaveMsg = realdata1.split('\\')[0]
 
             print("received message:", MESSAGE)
             print("tcpIP: ", TCP_IP)
             print("tcpPORT", TCP_PORT)
             print("N Next ", NR)
-            print('checksum', data[5][:-1], checksum(cmsg[0]))
-            if NS == int(not bool(NR) and int(checksum(cmsg[0])) == int(data[5][-1])):
+            print(MESSAGE)
+            print('checksum', data[5][:-1], checksum(MESSAGE))
+            if NS == int(not bool(NR)) and (checksum(MESSAGE) == data[5][:-1]):
+                print("heeeeeeeeeeeeereeeeeeeeeeeeee")
+                realdata = realdata + str(data[4][:-8])
+                realdata1 = realdata1 + str(data[0][2:]) + '@' + str(data[1]) + '@' + str(data[2]) + '@' + str(
+                    data[3]) + '@' + str(data[4])
+
+                cacheSaveMsg = realdata1.split('\\')[0]
+
                 UDP_PORT = 5008
                 ack = bytes(str(NR), 'utf-8')
+                print("NR:", ack, NR, NS)
                 sock = socket.socket(socket.AF_INET,  # Internet
                                      socket.SOCK_DGRAM)  # UDP
                 sock.sendto(ack, (UDP_IP, UDP_PORT))
@@ -125,18 +201,18 @@ if turn:
 
             if MF == 0:
                 break
-
-    print("real data : ", realdata[0:])
+    realdata += '\r\n\r\n'
+    print("real data : ", realdata)
 
 
     for i in httpCache:
         if cacheSaveMsg in i:
             print('here', httpCache)
-            responseToClient(i[1])
+            response_to_client(i[1])
             inCache = 1
 
 
-    if inCache == 0  : # if not in cache
+    if inCache == 0:  # if not in cache
 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print(TCP_IP)
@@ -150,25 +226,25 @@ if turn:
         # print('old cache',httpCache)
 
         # save data in cache
-        if len(httpCache) < 10 :
-            httpCache.append([cacheSaveMsg,data])
-        elif len(httpCache) == 10 :
+        if len(httpCache) < 10:
+            httpCache.append([cacheSaveMsg, data])
+        elif len(httpCache) == 10:
             del httpCache[index]
-            httpCache.insert(index,[cacheSaveMsg,data])
+            httpCache.insert(index, [cacheSaveMsg, data])
             index = index + 1
-            if index == 10 :
+            if index == 10:
                 index = 0
         # print('index',index)
         # print('new cache',httpCache)
 
-        responseToClient(data)
+        response_to_client(data)
 
     inCache = 0
 
 else:
 
     TCP_IP = bytes(IP, 'utf-8')
-    TCP_PORT = 5012
+    TCP_PORT = 5013
     BUFFER_SIZE = 1024  # Normally 1024, but we want fast response
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -185,9 +261,9 @@ else:
         else:
             print("received data:", data)
             data1 = str(data).split('@')
-            type = str(data1[0][2:])
+            dns_type = str(data1[0][2:])
             target = str(data1[1])
-            print('type: ', type)
+            print('type: ', dns_type)
             print('target: ', target)
             myResolver = dns.resolver.Resolver()  # create a new instance named 'myResolver'
             myResolver.timeout = 0.01
@@ -198,10 +274,10 @@ else:
                     conn.send(bytes(i[1], 'utf-8'))
                     inDNSCache = 1
 
-            if (inDNSCache == 0) :   #if not in cache
+            if inDNSCache == 0:   #if not in cache
                 while True:
                     try:
-                        myAnswers = myResolver.query(target, type)  # Lookup the 'A' record(s) for google.com
+                        myAnswers = myResolver.query(target, dns_type)  # Lookup the 'A' record(s) for google.com
                         break
                     except dns.exception.Timeout:
                         print('time out')
